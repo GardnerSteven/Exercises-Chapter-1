ae2baec6e50dda84f6b3ef352b3dcba4afa8dfcb
GardnerSteven <31359320+GardnerSteven@users.noreply.github.com>
2017-08-29 09:28:06 -0500

    Chapter 1 Exercises

---
+++ chapter1/src/excercises/MovieQuoteInfo.java
@@ -0,0 +1,14 @@
+package excercises;
+
+public class MovieQuoteInfo 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Are you too good for your home? Answer me!"
+				+" Movie Title:Happy Gilmore. Character:Happy Gilmore."
+				+" Year:1996");
+	}
+
+}
