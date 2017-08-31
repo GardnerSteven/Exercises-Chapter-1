48db50a22991ff73469ecda28faa952187180f3e
GardnerSteven <31359320+GardnerSteven@users.noreply.github.com>
2017-08-31 09:19:56 -0500

    Update

--- chapter1/src/debugging/FixDebugOne2.java
+++ chapter1/src/debugging/FixDebugOne2.java
@@ -8,7 +8,7 @@ public class FixDebugOne2
       System.out.println("Java programming is fun.");
       System.out.println("Getting a program to work");
       System.out.println("can be a challenge,");
-      System.out.println("but when everything works correctly");
-      System.out.println("it's very satisfying.");
+      System.out.println("but when everything works correctly,");
+      System.out.println("it's very satisfying");
    }
 }
--- chapter1/src/excercises/TableAndChairs.java
+++ chapter1/src/excercises/TableAndChairs.java
@@ -1,10 +1,17 @@
 package excercises;
 
-public class TableAndChairs {
+public class TableAndChairs 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("x                    x");
+		System.out.println("x                    x");
+		System.out.println("x     xxxxxxxxxx     x");
+		System.out.println("xxxxx x        x xxxxx");
+		System.out.println("x   x x        x x   x");
+		System.out.println("x   x x        x x   x");
+		
 	}
 
 }
