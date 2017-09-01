fe8a8b5dc53406fa886f6592e062024752a0e3af
GardnerSteven <31359320+GardnerSteven@users.noreply.github.com>
2017-09-01 08:43:13 -0500

    update

---
+++ chapter1/src/excercises/Comments.java
@@ -0,0 +1,15 @@
+package excercises;
+
+import javax.swing.JOptionPane;
+
+public class Comments 
+{
+
+	public static void main(String[] args)
+	{
+	JOptionPane.showMessageDialog(null,"Program comments are nonexecutiving statements you add to a file for the purpose of\r\n"+
+	"documentation.");	
+	System.out.println("Program comments are nonexecutiving statements you add to a file for the purpose of documentation.");
+	}
+
+}
---
+++ chapter1/src/excercises/FavoriteSong.java
@@ -0,0 +1,19 @@
+package excercises;
+
+public class FavoriteSong 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Man I just wanna go flex\r\n" + 
+				"Gold on my teeth and on my neck\r\n" + 
+				"And I'm stone cold with the flex\r\n" + 
+				"With my squad and I'm smokin' up a check\r\n" + 
+				"Man I just wanna go flex\r\n" + 
+				"Gold on my teeth and on my neck\r\n" + 
+				"And I'm stone cold with the flex\r\n" + 
+				"With my squad and I'm smokin' up a check");
+
+	}
+
+}
